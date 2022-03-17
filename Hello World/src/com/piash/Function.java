1a3bcdb4a2a5f03ca2368035c14d30c19e5445ba
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2022-03-17 11:48:14 +0600

    New code added

--- Hello World/src/com/piash/Function.java
+++ Hello World/src/com/piash/Function.java
@@ -1,4 +1,19 @@
 package com.piash;
 
+import java.util.Scanner;
+
 public class Function {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter  number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is :" +sum);
+
+
+    }
 }
