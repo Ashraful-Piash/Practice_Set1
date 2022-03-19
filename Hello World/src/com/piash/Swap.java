65c7453644b733954d09e63017d3652c9d19a36a
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2022-03-19 21:45:19 +0600

    New code added

---
+++ Hello World/src/com/piash/Swap.java
@@ -0,0 +1,15 @@
+package com.piash;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //swap numbers
+        int temp = a;
+        a= b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+
+}
