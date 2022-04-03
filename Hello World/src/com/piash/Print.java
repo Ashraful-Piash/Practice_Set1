06e198bfb25d5259b5e20da44c84a7ef9ca3f58c
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2022-04-03 23:31:13 +0600

    code added

--- Hello World/src/com/piash/Print.java
+++ Hello World/src/com/piash/Print.java
@@ -3,5 +3,6 @@ package com.piash;
 public class Print {
     public static void main(String[] args) {
         System.out.println("May ALLAH bless us all");
+        System.out.println("Ya ALLAH help me");
     }
 }
